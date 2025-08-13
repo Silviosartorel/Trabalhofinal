2e89ab6ef438bd84ba3dd075341566ed76f7331e
silvio <silviosartoreljr@gmail.com>
2025-08-13 09:35:59 -0300

    op

--- App.js
+++ App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './src/screens/HomeScreen';
-import ResultScreen from './src/screens/ResultScreen';
-import GraphScreen from './src/screens/GraphScreen';
+import HomeScreen from './components/HomeScreen.jsx';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,9 +9,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Result" component={ResultScreen} />
-        <Stack.Screen name="Graph" component={GraphScreen} />
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
